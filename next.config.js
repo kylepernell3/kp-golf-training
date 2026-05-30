50a82f3e6f2e796b3d251b6831bd3e6efba595cc
kylepernell3 <pernelldevelopment@gmail.com>
2026-05-30 17:33:53 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.cdn.filesafe.space',
+        pathname: '/**',
+      },
+    ],
+  },
+  // Allow .mov videos to be served
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
